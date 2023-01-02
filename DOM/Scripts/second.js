3388b486105c3ed7d19b78ea44ee155965977301
TANI TARA <83660067+Tani1814@users.noreply.github.com>
2023-01-02 22:13:01 +0600

    Add files via upload

---
+++ DOM/Scripts/second.js
@@ -0,0 +1,7 @@
+console.log('if you want to use so many scripts in one code you can use it like this');
+//make a folder
+//keep scripts file there
+//link them with that html file 
+//just like if you would use a single script file
+//check index.html
+//and remember script tag sequence maintain kore output dey
